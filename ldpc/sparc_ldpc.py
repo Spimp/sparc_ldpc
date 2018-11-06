94cdf57c04dfafc5d7ff07e8569b7e3653c66f5d
spimp <ks760@cam.ac.uk>
2018-11-06 13:49:07 +0000

    now plotting rate against BER for just sparc. Has 100 repeats of this code. Saves the resulting plot instead of displaying it.

--- ldpc/sparc_ldpc.py
+++ ldpc/sparc_ldpc.py
@@ -2,6 +2,8 @@ import numpy as np
 import math as math
 from pylab import *
 import py.ldpc as ldpc
+import matplotlib.pyplot as plt
+import time
 
 
 # Code taken from Adam's python tutorial. 
@@ -151,7 +153,9 @@ def amp(y, σ_n, Pl, L, M, T, Ab, Az):
     
     for t in range(T):
         τ = np.sqrt(np.sum(z**2)/n)
-        if τ == last_τ:
+        #if τ == last_τ:
+        # can change params for isclose if need be. If stopping too early.
+        if np.isclose(τ, last_τ):
             return β
         last_τ = τ
         
@@ -175,13 +179,13 @@ def amp(y, σ_n, Pl, L, M, T, Ab, Az):
 # r is the user data rate, r_pa the power allocation design rate
 # t is the maximum number of AMP iterations permitted
 class SPARCParams:
-	def __init__(self, L, M, sigma, p, r, r_pa, t):
+	def __init__(self, L, M, sigma, p, r, t):
 		self.L = L
 		self.M = M
 		self.sigma = sigma
 		self.p = p
 		self.r = r
-		self.r_pa = r_pa
+		#self.r_pa = r_pa
 		self.t = t
 
 # LDPC Params needed to set up one of Jossy's code
@@ -216,6 +220,8 @@ class LDPCResult:
 def sp2bp(β, L, M: "must be power of 2"):
     #take in the section posterior probabilities β.
     #and the no. sections L and the size of each section M. 
+    #note L may not equal the total number of sections for the sparc code. 
+    #normally just interested in the number of sections covered by the LDPC
     
     #initialise numpy array of zeros for all of the bitwise posteriors
     p = np.zeros(int(np.log2(M)*L))
@@ -262,6 +268,7 @@ def bits2indices(bits, m: "m must be a power of 2"):
 
     return indices
 
+# note removed r_pa from function as never use it
 def amp_ldpc_sim(sparcparams: SPARCParams, ldpcparams: LDPCParams = None):
     #Get the SPARC parameters from the struct
     L = sparcparams.L
@@ -269,7 +276,7 @@ def amp_ldpc_sim(sparcparams: SPARCParams, ldpcparams: LDPCParams = None):
     P = sparcparams.p
     sigma = sparcparams.sigma
     r_sparc = sparcparams.r
-    r_pa_sparc = sparcparams.r_pa
+    #r_pa_sparc = sparcparams.r_pa
     T = sparcparams.t
 
     # calculate some additional parameters 
@@ -337,7 +344,7 @@ def amp_ldpc_sim(sparcparams: SPARCParams, ldpcparams: LDPCParams = None):
         β_0[l*M + sparc_indices[l]] = np.sqrt(n * Pl[l])
     x = Ab(β_0)
     
-    # check that the power has been allocated uniformly. This should be approx equal to the snr. 
+    # check that the power has been allocated uniformly. This should be approx equal to one when divided by the snr. 
     print("Average power/snr is ", np.mean(x**2)/snr)
 
     # Generate random channel noise and then received signal y
@@ -373,19 +380,18 @@ def amp_ldpc_sim(sparcparams: SPARCParams, ldpcparams: LDPCParams = None):
         ldpc_sections = int(nl/logm)
         # convert sectionwise to bitwise posterior probabilities
         # only need the sections corresponding to ldpc code 
-        bitwise_posterior = sp2bp(sectionwise_posterior[(L-ldpc_sections)*M:], L, M) 
+        bitwise_posterior = sp2bp(sectionwise_posterior[(L-ldpc_sections)*M:], ldpc_sections, M) 
 
         # recenter the bitwise posterior around zero for the decoding
         LLR = np.log(1-bitwise_posterior)- np.log(bitwise_posterior)
 
         (app, it) = ldpc_code.decode(LLR)
-        #print((np.ones(nl)/2)-bitwise_posterior[int((L*logm)-nl):])
+
         test_output = (bitwise_posterior>0.5)
         print("Incorrect bits before ldpc decoding: ", np.sum(test_output!=ldpc_bits))
 
         # check that the inequality is the right way round in below
         v_output = (app<0.0)
-
         print("Incorrect bits in ldpc decoding: ", np.sum(v_output!=ldpc_bits))
 
         # convert the bits to indices
@@ -406,7 +412,7 @@ def amp_ldpc_sim(sparcparams: SPARCParams, ldpcparams: LDPCParams = None):
     R = (L*logm - (nl-kl))/n
 
     #Compute Eb/N0
-    EbN0 = 1/(2*R) * (P/sigma**2)
+    #EbN0 = 1/(2*R) * (P/sigma**2)
    
     return ber_amp, ber_ldpc, R
 
@@ -414,25 +420,24 @@ def amp_ldpc_sim(sparcparams: SPARCParams, ldpcparams: LDPCParams = None):
 
 
 if __name__ == "__main__":
+    # get the time so you can calculate the wall clock time of the process
+    t0 = time.time()
     '''c = np.array([0.2, 0.3, 0.1, 0.4, 0.6, 0.4, 0, 0])
     print(sp2bp(c, 2, 4))
 
-    '''
+    ##############################################################
     # Number of sections covered by ldpc code vs. BER.    
     
-    M = 512
+    '''
+    '''M = 512
     L = 512
     sigma = 1
-    P = 4
-    r_sparc = 1
-    r_pa_sparc = 1
+    P = 3
+    r_sparc = 0.75
+    # removed r_pa from function as never us it
+    #r_pa_sparc = 1
     T = 64
-    sparcparams = SPARCParams(L, M, sigma, P, r_sparc, r_pa_sparc, T)
-    
-    # This should give increments of 64 each time
-    # Ensures number of sections is divisible by 8. 
-    #ldpc_sections = linspace(0, 512, 9)
-
+    sparcparams = SPARCParams(L, M, sigma, P, r_sparc, T)
     
     # The ldpc sections must be less than L and must be divisible by 8 to give an integer value for z
     ldpc_sections = 400
@@ -446,34 +451,204 @@ if __name__ == "__main__":
     rate = '5/6'
     ldpcparams = LDPCParams(standard, rate, z)
 
-    (ber_amp, ber_ldpc, R) = amp_ldpc_sim(sparcparams, ldpcparams)
+    (ber_amp, ber_ldpc, R) = amp_ldpc_sim(sparcparams)#, ldpcparams)
     print("ber_amp: ", ber_amp)
-    print("ber_ldpc: ", ber_ldpc)
+    #print("ber_ldpc: ", ber_ldpc)
     print("R: ", R)
 
+    datapoints = 9
+    # This should give increments of 64 each time
+    # Ensures number of sections is divisible by 8. 
+    ldpc_sections = linspace(0, 512, datapoints)
 
+    ber_ldpc = np.zeros(datapoints)
+    R_overall = np.zeros(datapoints)
+    i = 0
+    repeats = 10
+    M = 512
+    L = 1024
+    logm = int(np.log2(M))
+    # Overall rate R
+    R = 0.75
+    for sec in ldpc_sections:
+        #number of LDPC bits. Must be divisible by 8.
+        nl = logm * sec
+        print(nl)
+        # Can get from nl to z. z determines the size of the protograph matrix
+        z = int(nl/24)
+        standard = '802.16'
+        # if change this, change formula for kl
+        rate = '2/3'
+        
+        ldpcparams = LDPCParams(standard, rate, z)
+        # note 2/3 is the rate of the ldpc. Hard coded this to avoid rounding errors in storing this
+        kl = nl * 2/3
+        n = (L*logm - (nl-kl))/R
+        # overall rate of the ldpc/ sparc code. 
+        r_sparc = (L*logm)/n
+
+
+        sigma = 1
+        P = 2.3
+        T = 64
+        # sparc params for the ldpc/ sparc code
+        sparcparams_ldpc = SPARCParams(L, M, sigma, P, r_sparc, T)
+
+        ber_ldpc_rep = np.zeros(repeats)
+        R_overall_rep = np.zeros(repeats)
+        for j in range(repeats):
+            if z>0:
+                (_, ber_ldpc_rep[j], R_overall_rep[j]) = amp_ldpc_sim(sparcparams_ldpc, ldpcparams)
+            else:
+                # if z=0 then just doing sparc with no ldpc
+                (ber_ldpc_rep[j], _, R_overall_rep[j]) = amp_ldpc_sim(sparcparams_ldpc)
+        ber_ldpc[i] = np.sum(ber_ldpc_rep)/repeats
+        R_overall[i] = np.sum(R_overall_rep)/repeats
+        i += 1
+    print(R_overall)
+    plot(ldpc_sections, ber_ldpc)
+    xlabel('Sections covered by ldpc')
+    ylabel('BER')
+    show()'''
+    '''################################################################
+    # Keep the sparc rate constant and then the snr we choose will give the same
+    # BER each time. But vary the number of sections covered. This will vary the overall rate
+    # Compare to a sparc code with the overall rate and see which performs better. 
+    datapoints = 17
+    # This should give increments of 64 each time
+    # Ensures number of sections is divisible by 8. 
+    ldpc_sections = linspace(0, 1024, datapoints)
+    print(ldpc_sections)
+
+    ber_ldpc = np.zeros(datapoints)
+    ber_noldpc = np.zeros(datapoints)
+    R_overall = np.zeros(datapoints)
+    i = 0
+    repeats = 1
+    M = 512
+    L = 1024
+    logm = int(np.log2(M))
+    # sparc rate
+    r_sparc = 0.75
+    for sec in ldpc_sections:
+        #number of LDPC bits. Must be divisible by 8.
+        nl = logm * sec
+        print(nl)
+        # Can get from nl to z. z determines the size of the protograph matrix
+        z = int(nl/24)
+        standard = '802.16'
+        # if change this, change formula for kl
+        rate = '5/6'
+        
+        ldpcparams = LDPCParams(standard, rate, z)
+
+        sigma = 1
+        P = 2.4
+        T = 64
+        # sparc params for the ldpc/ sparc code
+        sparcparams_ldpc = SPARCParams(L, M, sigma, P, r_sparc, T)
+        ber_ldpc_rep = np.zeros(repeats)
+        ber_noldpc_rep = np.zeros(repeats)
+        R_overall_rep = np.zeros(repeats)
+        for j in range(repeats):
+            if z>0:
+                (_, ber_ldpc_rep[j], R_overall_rep[j]) = amp_ldpc_sim(sparcparams_ldpc, ldpcparams)
+                # calculate the BER for a sparc with the same overall rate but no ldpc code.
+                sparcparams_noldpc = SPARCParams(L, M, sigma, P, R_overall_rep[j], T)
+                (ber_noldpc_rep[j],_,_) = amp_ldpc_sim(sparcparams_noldpc)
+            else:
+                # if z=0 then just doing sparc with no ldpc
+                (ber_ldpc_rep[j], _, R_overall_rep[j]) = amp_ldpc_sim(sparcparams_ldpc)
+                ber_noldpc_rep[j] = ber_ldpc_rep[j]
+        ber_ldpc[i] = np.sum(ber_ldpc_rep)/repeats
+        R_overall[i] = np.sum(R_overall_rep)/repeats
+        ber_noldpc[i] = np.sum(ber_noldpc_rep)/repeats
+        i += 1
+
+    fig, ax = plt.subplots()
+    ax.set_yscale('log', basey=10)
+    ax.plot(R_overall, ber_ldpc, 'k--', label = 'With ldpc')
+    ax.plot(R_overall, ber_noldpc, 'k:', label = 'Without ldpc')
+    plt.xlabel('Overall Rate')
+    plt.ylabel('BER')
+    plt.legend()
+    plt.title('Trade off with using lower overall rate to cover more sections with ldpc or just having lower sparc rate.')
+    plt.show()
+    print("Wall clock time elapsed: ", time.time()-t0)
     '''
-    #Code to plot the performance of the sparc code on it's own. 
+
+
+    #########################################################
+    # plot of performance of sparc code on it's own with a decreasing rate. 
+    # Sparc parameters
+    L = 1024
+    M = 512
+    sigma = 1
+    # 3 different values of power to test. 
+    P1 = 2.0
+    P2 = 2.2 
+    P3 = 2.4
+    T = 64
+
+    repeats = 100
+    datapoints = 6
+    R = linspace(0.5, 0.75, datapoints)
+    BER1 = np.zeros(datapoints)
+    BER2 = np.zeros(datapoints)
+    BER3 = np.zeros(datapoints)
+    i=0
+    for r_sparc in R:
+        sparc_params1 = SPARCParams(L, M, sigma, P1, r_sparc, T)
+        sparc_params2 = SPARCParams(L, M, sigma, P2, r_sparc, T)
+        sparc_params3 = SPARCParams(L, M, sigma, P3, r_sparc, T)
+        BER1_rep = np.zeros(repeats)
+        BER2_rep = np.zeros(repeats)
+        BER3_rep = np.zeros(repeats)
+        for j in range(repeats):
+            (BER1_rep[j],_,_) = amp_ldpc_sim(sparc_params1)
+            (BER2_rep[j],_,_) = amp_ldpc_sim(sparc_params2)
+            (BER3_rep[j],_,_) = amp_ldpc_sim(sparc_params3)
+        BER1[i] = np.sum(BER1_rep)/repeats
+        BER2[i] = np.sum(BER2_rep)/repeats
+        BER3[i] = np.sum(BER3_rep)/repeats
+        i+=1
+    fig, ax = plt.subplots()
+    ax.set_yscale('log', basey=10)
+    ax.plot(R, BER1, 'k--', label = 'Power=2.0')
+    ax.plot(R, BER2, 'k:', label = 'Power=2.2')
+    ax.plot(R, BER3, 'k-.', label = 'Power=2.4')
+    plt.xlabel('Overall Rate')
+    plt.ylabel('BER')
+    plt.legend()
+    plt.title("BER of the SPARC code as rate varies")
+    print("Wall clock time elapsed: ", time.time()-t0)
+    plt.savefig('sparc_RVsBER_1.png')
+    
+    '''
+    #################################################################
+    #Code to plot the performance of the sparc code on it's own for 
+    # a fixed rate and varying snr. 
+    L = 1024
+    M = 512
     # fix rate
-    R = 1
+    R = 0.75
 
     σ_n = 1.0
-    # shannon limit on snr. i.e. snr at capacity
+    # shannon limit on snr. i.e. snr at capacity, where R is capacity.
     snrc = 2**(2*R)-1
     
     # Unsure what values to choose for T and R_PA. Just copied previous example.
     T = 64
-    R_PA = R
     # store the error rate for each snr
     error_rate = []
-    repeats = 10
+    repeats = 1
     snr_array = np.linspace(snrc, snrc+1, 11)
     print(snr_array)
     for snr in snr_array:
         # calculate the new value of P to adjust the snr
         P = snr * σ_n**2
         error = []
-        sparcparams = SPARCParams(512, 512, 1, P, 1, 1, 64)
+        sparcparams = SPARCParams(L, M, σ_n, P, R, T)
         for i in range(repeats):
             ber = amp_ldpc_sim(sparcparams)[0]
             error.append(ber)
@@ -482,7 +657,7 @@ if __name__ == "__main__":
         
     plot(snr_array, error_rate)
     xlabel('snr')
-    title("Parameters: L=512, M=512, σ_n=1, R=1, T=64, R_PA=1")
+    title("Parameters: L=1024, M=512, σ_n=1, R=0.75, T=64")
     ylabel('BER')
     show()'''
 
